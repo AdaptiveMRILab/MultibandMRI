63aa8fd30c4dcf20f4f0410bf626bdc3b32a1b18
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-21 10:52:53 -0600

    bug fix in slice_grappa 5

--- MultibandMRI/slice_grappa.py
+++ MultibandMRI/slice_grappa.py
@@ -56,7 +56,7 @@ class slice_grappa:
             y = y.view(self.sms, self.coils, nread, -1)
             Y.append(y) 
         #ny = sum([Y[r].shape[-1] for r in range(self.phase_accel)])
-        out = torch.zeros((self.sms, self.coils, nread, Y[0].shape), dtype=data.dtype, device=data.device)
+        out = torch.zeros((self.sms, self.coils, nread, Y[0].shape[-1]), dtype=data.dtype, device=data.device)
         for r in range(self.phase_accel):
             out[:,:,:,r::self.phase_accel] = Y[r][:,:,:,r::self.phase_accel]
         return out 
