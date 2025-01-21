7863267f97a876c973f2c9b668de550a061f6edb
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-21 11:01:47 -0600

    bug fix in slice_grappa 6

--- MultibandMRI/slice_grappa.py
+++ MultibandMRI/slice_grappa.py
@@ -45,18 +45,18 @@ class slice_grappa:
     def apply(self, data):
         '''
         Input:  
-            data: zero-filled measured SMS data (1, coils, readout, phase) complex64 tensor
+            data: measured SMS data (1, coils, readout, phase) complex64 tensor
         '''
 
         nread = data.shape[2]
-        A = extract_and_flatten_multicoil_kspace_patches(data, kernel_size=self.kernel_size, stride=self.stride, pad=True)[:,None,:,:]
+        A = extract_and_flatten_multicoil_kspace_patches(data, kernel_size=self.kernel_size, stride=(1,1), pad=True)[:,None,:,:]
         Y = []
         for r in range(self.phase_accel):
             y = A @ self.weights[r]
             y = y.view(self.sms, self.coils, nread, -1)
             Y.append(y) 
-        #ny = sum([Y[r].shape[-1] for r in range(self.phase_accel)])
-        out = torch.zeros((self.sms, self.coils, nread, Y[0].shape[-1]), dtype=data.dtype, device=data.device)
+        ny = sum([Y[r].shape[-1] for r in range(self.phase_accel)])
+        out = torch.zeros((self.sms, self.coils, nread, ny), dtype=data.dtype, device=data.device)
         for r in range(self.phase_accel):
-            out[:,:,:,r::self.phase_accel] = Y[r][:,:,:,r::self.phase_accel]
+            out[:,:,:,r::self.phase_accel] = Y[r]
         return out 
