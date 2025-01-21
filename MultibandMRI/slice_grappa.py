5c12ae61b887c54dc20e6d9270efd230c75ceab5
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-21 17:21:37 -0600

    final matrix size adjustment to slice grappa

--- MultibandMRI/slice_grappa.py
+++ MultibandMRI/slice_grappa.py
@@ -10,7 +10,8 @@ class slice_grappa:
                  calib_data: Tensor,
                  accel: Tuple=(1,1),
                  kernel_size: Tuple=(3,3),
-                 tik: float=0.0):
+                 tik: float=0.0,
+                 final_matrix_size: Tuple=None):
         '''
         Input:
             calib_data: (sms, coils, readout, phase) complex64 tensor
@@ -22,6 +23,7 @@ class slice_grappa:
         self.accel = accel
         self.kernel_size = kernel_size 
         self.tik = tik 
+        self.final_matrix_size = final_matrix_size
         self.calibrate(calib_data)
 
     def calibrate(self, calib_data):
@@ -51,6 +53,7 @@ class slice_grappa:
 
     def apply(self, data):
 
+        # figure out number of interpolated points along each dimension 
         rows, cols = data.shape[2], data.shape[3]
         eff_row_kernel_size = (self.kernel_size[0] - 1) * self.accel[0] + 1
         eff_col_kernel_size = (self.kernel_size[1] - 1) * self.accel[1] + 1
@@ -61,88 +64,22 @@ class slice_grappa:
         nr = torch.numel(rv)
         nc = torch.numel(cv)
 
+        # interpolate the missing points
         A = get_kernel_patches(data, kernel_size=self.kernel_size, accel=self.accel, stride=self.accel)
         Y = [(A@w).view(self.sms, self.coils, nr, -1) for w in self.weights]
         out = torch.zeros_like(Y[0])
         for rfe in range(self.accel[0]):
             for rpe in range(self.accel[1]):
                 out[:,:,rfe::self.accel[0],rpe::self.accel[1]] = Y[rfe*self.accel[1]+rpe][:,:,0::self.accel[0],0::self.accel[1]]
+        
+        # zero-fill to final matrix size 
+        if self.final_matrix_size is not None:
+            rowpad = self.final_matrix_siz[0] - out.shape[2]
+            rowpre = rowpad//2
+            rowpst = rowpad - rowpre 
+            colpad = self.final_matrix_size[1] - out.shape[3]
+            colpre = colpad//2
+            colpst = colpad - colpre 
+            out = torch.nn.functional.pad(out, (colpre, colpst, rowpre, rowpst), mode='constant', value=0)
+        
         return out
-
-
-
-
-
-# class slice_grappa:
-
-#     def __init__(self,
-#                  calib_data: Tensor,
-#                  phase_accel: int=1,
-#                  kernel_size: Tuple=(3,3),
-#                  tik: float=0.0):
-#         '''
-#         Input:
-#             calib_data: (sms, coils, readout, phase) complex64 tensor
-#             phase_accel: integer (uniform) acceleration factor 
-#             kernel_size: tuple of (readout, phase) kernel sizes
-#         '''
-
-#         self.sms, self.coils, _, _ = calib_data.shape
-#         self.phase_accel = int(phase_accel) 
-#         self.kernel_size = kernel_size 
-#         self.stride = (1, self.phase_accel)
-#         self.tik = tik 
-#         self.calibrate(calib_data)
-
-#     def calibrate(self, calib_data):
-
-#         source = torch.sum(calib_data, dim=0, keepdim=True)
-#         A = extract_and_flatten_multicoil_kspace_patches(source, kernel_size=self.kernel_size, stride=self.stride)[:,None,:,:]
-#         if self.tik > 0.0:
-#             AH = A.conj().transpose(2,3)
-#             I = torch.eye(AH.shape[0], dtype=A.dtype, device=A.device)[None,None,:,:]
-#             AHA = AH@A + self.tik*I 
-#         self.weights = []
-#         fe_shift = self.kernel_size[0] // 2
-#         pe_shift = (self.kernel_size[0]-1)*self.phase_accel // 2 - 1
-#         for r in range(self.phase_accel):
-#             shift = (fe_shift, pe_shift+r)
-#             print(shift)
-#             b = extract_point_within_multicoil_kspace_patch(calib_data, kernel_size=self.kernel_size, stride=self.stride, shift=shift)[...,None]
-#             if self.tik > 0.0:
-#                 X = torch.linalg.inv(AHA) @ (AH @ b)
-#             else:
-#                 X = torch.linalg.lstsq(A, b, rcond=None)[0]
-#             self.weights.append(X)
-
-#     def apply(self, data):
-#         '''
-#         Input:  
-#             data: zero-filled measured SMS data (1, coils, readout, phase) complex64 tensor
-#         '''
-
-#         # nread = data.shape[2]
-#         # A = extract_and_flatten_multicoil_kspace_patches(data, kernel_size=self.kernel_size, stride=self.stride, pad=True)[:,None,:,:]
-#         # Y = []
-#         # for r in range(self.phase_accel):
-#         #     y = A @ self.weights[r]
-#         #     y = y.view(self.sms, self.coils, nread, -1)
-#         #     Y.append(y) 
-#         # ny = sum([Y[r].shape[-1] for r in range(self.phase_accel)])
-#         # out = torch.zeros((self.sms, self.coils, nread, ny), dtype=data.dtype, device=data.device)
-#         # for r in range(self.phase_accel):
-#         #     out[:,:,:,r::self.phase_accel] = Y[r]
-#         # return out 
-
-#         nread = data.shape[2]
-#         A = extract_and_flatten_multicoil_kspace_patches(data, kernel_size=self.kernel_size, stride=(1,1), pad=True)[:,None,:,:]
-#         Y = []
-#         for r in range(self.phase_accel):
-#             y = A @ self.weights[r]
-#             y = y.view(self.sms, self.coils, nread, -1)
-#             Y.append(y) 
-#         ny = sum([Y[r].shape[-1] for r in range(self.phase_accel)])
-#         out = torch.zeros((self.sms, self.coils, nread, ny), dtype=data.dtype, device=data.device)
-#         for r in range(self.phase_accel):
-#             out[:,:,:,r::self.phase_accel] = Y[r]
-#         return out 
