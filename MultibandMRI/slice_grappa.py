971ade8b4f56c60c62cb28c1a166ff76dd7805f0
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-21 13:44:50 -0600

    slice grappa overhaul

--- MultibandMRI/slice_grappa.py
+++ MultibandMRI/slice_grappa.py
@@ -1,13 +1,14 @@
 import torch 
 from torch import Tensor
 from typing import Tuple 
-from MultibandMRI import extract_and_flatten_multicoil_kspace_patches, extract_point_within_multicoil_kspace_patch
+# from MultibandMRI import extract_and_flatten_multicoil_kspace_patches, extract_point_within_multicoil_kspace_patch
+from MultibandMRI import get_kspace_patches, get_kspace_points
 
 class slice_grappa:
 
     def __init__(self,
                  calib_data: Tensor,
-                 phase_accel: int=1,
+                 accel: Tuple=(1,1),
                  kernel_size: Tuple=(3,3),
                  tik: float=0.0):
         '''
@@ -18,61 +19,119 @@ class slice_grappa:
         '''
 
         self.sms, self.coils, _, _ = calib_data.shape
-        self.phase_accel = int(phase_accel) 
+        self.accel = accel
         self.kernel_size = kernel_size 
-        self.stride = (1, self.phase_accel)
         self.tik = tik 
         self.calibrate(calib_data)
 
     def calibrate(self, calib_data):
-
+        
+        # "source" data for slice grappa calibration is the multiband k-space 
         source = torch.sum(calib_data, dim=0, keepdim=True)
-        A = extract_and_flatten_multicoil_kspace_patches(source, kernel_size=self.kernel_size, stride=self.stride)[:,None,:,:]
+        A = get_kspace_patches(source, kernel_size=self.kernel_size, accel=self.accel)
+
+        # if l2 regularization is desired 
         if self.tik > 0.0:
             AH = A.conj().transpose(2,3)
             I = torch.eye(AH.shape[0], dtype=A.dtype, device=A.device)[None,None,:,:]
-            AHA = AH@A + self.tik*I 
+            AHA_inv = torch.linalg.inv(AH@A + self.tik*I)
+
+        # calculate the weights for each offset relative to "top left" kernel
+        # point (i.e., to account for in-plane acceleration)
         self.weights = []
-        fe_shift = self.kernel_size[0] // 2
-        pe_shift = (self.kernel_size[0]-1)*self.phase_accel // 2 - 1
-        for r in range(self.phase_accel):
-            shift = (fe_shift, pe_shift+r)
-            print(shift)
-            b = extract_point_within_multicoil_kspace_patch(calib_data, kernel_size=self.kernel_size, stride=self.stride, shift=shift)[...,None]
-            if self.tik > 0.0:
-                X = torch.linalg.inv(AHA) @ (AH @ b)
-            else:
-                X = torch.linalg.lstsq(A, b, rcond=None)[0]
-            self.weights.append(X)
+        base_read_shift = (self.kernel_size[0] * self.accel[0])//2 
+        base_phase_shift = (self.kernel_size[1] * self.accel[1])//2
+        for rfe in range(self.accel[0]):
+            for rpe in range(self.accel[1]):
+                shifts = (base_read_shift+rfe, base_phase_shift+rpe)
+                b = get_kspace_points(calib_data, shifts=shifts, kernel_size=self.kernel_size, accel=self.accel)
+                self.weights.append(AHA_inv @ (AH @ b))
 
     def apply(self, data):
-        '''
-        Input:  
-            data: zero-filled measured SMS data (1, coils, readout, phase) complex64 tensor
-        '''
-
-        # nread = data.shape[2]
-        # A = extract_and_flatten_multicoil_kspace_patches(data, kernel_size=self.kernel_size, stride=self.stride, pad=True)[:,None,:,:]
-        # Y = []
-        # for r in range(self.phase_accel):
-        #     y = A @ self.weights[r]
-        #     y = y.view(self.sms, self.coils, nread, -1)
-        #     Y.append(y) 
-        # ny = sum([Y[r].shape[-1] for r in range(self.phase_accel)])
+        nread = data.shape[2]
+        A = get_kspace_patches(data, kernel_size=self.kernel_size, accel=self.accel, pad=True)
+        Y = [(A@w).view(self.sms, self.coils, nread, -1) for w in self.weights]
+        for n in range(len(Y)):
+            print(Y.shape)
         # out = torch.zeros((self.sms, self.coils, nread, ny), dtype=data.dtype, device=data.device)
         # for r in range(self.phase_accel):
         #     out[:,:,:,r::self.phase_accel] = Y[r]
-        # return out 
+        return []
 
-        nread = data.shape[2]
-        A = extract_and_flatten_multicoil_kspace_patches(data, kernel_size=self.kernel_size, stride=(1,1), pad=True)[:,None,:,:]
-        Y = []
-        for r in range(self.phase_accel):
-            y = A @ self.weights[r]
-            y = y.view(self.sms, self.coils, nread, -1)
-            Y.append(y) 
-        ny = sum([Y[r].shape[-1] for r in range(self.phase_accel)])
-        out = torch.zeros((self.sms, self.coils, nread, ny), dtype=data.dtype, device=data.device)
-        for r in range(self.phase_accel):
-            out[:,:,:,r::self.phase_accel] = Y[r]
-        return out 
+
+
+
+
+# class slice_grappa:
+
+#     def __init__(self,
+#                  calib_data: Tensor,
+#                  phase_accel: int=1,
+#                  kernel_size: Tuple=(3,3),
+#                  tik: float=0.0):
+#         '''
+#         Input:
+#             calib_data: (sms, coils, readout, phase) complex64 tensor
+#             phase_accel: integer (uniform) acceleration factor 
+#             kernel_size: tuple of (readout, phase) kernel sizes
+#         '''
+
+#         self.sms, self.coils, _, _ = calib_data.shape
+#         self.phase_accel = int(phase_accel) 
+#         self.kernel_size = kernel_size 
+#         self.stride = (1, self.phase_accel)
+#         self.tik = tik 
+#         self.calibrate(calib_data)
+
+#     def calibrate(self, calib_data):
+
+#         source = torch.sum(calib_data, dim=0, keepdim=True)
+#         A = extract_and_flatten_multicoil_kspace_patches(source, kernel_size=self.kernel_size, stride=self.stride)[:,None,:,:]
+#         if self.tik > 0.0:
+#             AH = A.conj().transpose(2,3)
+#             I = torch.eye(AH.shape[0], dtype=A.dtype, device=A.device)[None,None,:,:]
+#             AHA = AH@A + self.tik*I 
+#         self.weights = []
+#         fe_shift = self.kernel_size[0] // 2
+#         pe_shift = (self.kernel_size[0]-1)*self.phase_accel // 2 - 1
+#         for r in range(self.phase_accel):
+#             shift = (fe_shift, pe_shift+r)
+#             print(shift)
+#             b = extract_point_within_multicoil_kspace_patch(calib_data, kernel_size=self.kernel_size, stride=self.stride, shift=shift)[...,None]
+#             if self.tik > 0.0:
+#                 X = torch.linalg.inv(AHA) @ (AH @ b)
+#             else:
+#                 X = torch.linalg.lstsq(A, b, rcond=None)[0]
+#             self.weights.append(X)
+
+#     def apply(self, data):
+#         '''
+#         Input:  
+#             data: zero-filled measured SMS data (1, coils, readout, phase) complex64 tensor
+#         '''
+
+#         # nread = data.shape[2]
+#         # A = extract_and_flatten_multicoil_kspace_patches(data, kernel_size=self.kernel_size, stride=self.stride, pad=True)[:,None,:,:]
+#         # Y = []
+#         # for r in range(self.phase_accel):
+#         #     y = A @ self.weights[r]
+#         #     y = y.view(self.sms, self.coils, nread, -1)
+#         #     Y.append(y) 
+#         # ny = sum([Y[r].shape[-1] for r in range(self.phase_accel)])
+#         # out = torch.zeros((self.sms, self.coils, nread, ny), dtype=data.dtype, device=data.device)
+#         # for r in range(self.phase_accel):
+#         #     out[:,:,:,r::self.phase_accel] = Y[r]
+#         # return out 
+
+#         nread = data.shape[2]
+#         A = extract_and_flatten_multicoil_kspace_patches(data, kernel_size=self.kernel_size, stride=(1,1), pad=True)[:,None,:,:]
+#         Y = []
+#         for r in range(self.phase_accel):
+#             y = A @ self.weights[r]
+#             y = y.view(self.sms, self.coils, nread, -1)
+#             Y.append(y) 
+#         ny = sum([Y[r].shape[-1] for r in range(self.phase_accel)])
+#         out = torch.zeros((self.sms, self.coils, nread, ny), dtype=data.dtype, device=data.device)
+#         for r in range(self.phase_accel):
+#             out[:,:,:,r::self.phase_accel] = Y[r]
+#         return out 
--- MultibandMRI/utils.py
+++ MultibandMRI/utils.py
@@ -1,5 +1,6 @@
 import torch 
 from torch import Tensor 
+from typing import Tuple 
 
 def fft1d(inp, dim):
     return torch.fft.fftshift(torch.fft.fft(torch.fft.fftshift(inp, dim=dim), dim=dim), dim=dim)
@@ -12,6 +13,80 @@ def fft2d(inp, dims=(0,1)):
 
 def ifft2d(inp, dims=(0,1)):
     return ifft1d(ifft1d(inp, dims[0]), dims[1])
+
+def get_kspace_patches(
+        inp: Tensor, # [batch_size, coils, rows, cols]
+        kernel_size=(5,5),
+        accel=(1,1),
+        pad=False
+):
+    
+    coils = inp.shape[1]
+    
+    # get the stride=1 total kernel sizes 
+    kern_rows = kernel_size[0]*accel[0] - 1
+    kern_cols = kernel_size[1]*accel[1] - 1
+    ksize = (kern_rows, kern_cols)
+
+    if pad:
+        K,L = ksize 
+        pad_height_total = K - 1
+        pad_width_total = L - 1
+        pad_top = pad_height_total // 2
+        pad_bottom = pad_height_total - pad_top
+        pad_left = pad_width_total // 2
+        pad_right = pad_width_total - pad_left
+        inp = torch.nn.functional.pad(inp, (pad_left, pad_right, pad_top, pad_bottom), mode='constant', value=0)
+    
+    # source point mask 
+    mask = torch.zeros((1, coils, kern_rows, kern_cols), dtype=torch.int32, device=inp.device)
+    mask[:,:,::accel[0],::accel[1]] = 1
+    mask = torch.nn.functional.unfold(mask, kernel_size=ksize)
+    mask = mask[0,:,0]
+    indices = torch.nonzero(mask).squeeze()
+
+    # get multi-coil patches of k-space from source points 
+    # these patches will have size [batch_size, num_patches, coils*kernel_size[0]*kernel_size[1]]
+    patches = torch.nn.functional.unfold(inp, kernel_size=ksize)
+    patches = patches[:,indices,:]
+
+    # return the patches with a singleton channel dimension 
+    return patches[:,None,:,:]
+
+def get_kspace_points(
+        inp: Tensor,
+        shifts: Tuple,
+        kernel_size=(5,5),
+        accel=(1,1),
+):
+    
+    coils = inp.shape[1]
+    
+    # get the stride=1 total kernel sizes 
+    kern_rows = kernel_size[0]*accel[0] - 1
+    kern_cols = kernel_size[1]*accel[1] - 1
+    ksize = (kern_rows, kern_cols)
+
+    # source point mask 
+    mask = torch.zeros((1, coils, kern_rows, kern_cols), dtype=torch.int32, device=inp.device)
+    mask[:,:,shifts[0],shifts[1]] = 1
+    mask = torch.nn.functional.unfold(mask, kernel_size=ksize)
+    mask = mask[0,:,0]
+    indices = torch.nonzero(mask).squeeze()
+
+    # get point within multi-coil patches of k-space
+    patches = torch.nn.functional.unfold(inp, kernel_size=ksize)
+    patches = patches[:,indices,:] 
+
+    # return the patches with a singleton 4th dimension
+    # it will have a shape [batch_size, coils, num_kernels, 1]
+    return patches[...,None]
+
+
+
+
+    
+   
 
 def extract_and_flatten_multicoil_kspace_patches(
         inp: Tensor,       # k-space tensor of shape [batch_size, coils, nfe, np]
