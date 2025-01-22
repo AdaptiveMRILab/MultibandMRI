eecb6df47c13910fb9b18d6f6f40baf0294568d7
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-22 14:58:05 -0600

    sense grappa bug fix 14

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -74,7 +74,8 @@ class sense_grappa:
 
         # interpolate the missing points
         A = get_kernel_patches(data, kernel_size=self.kernel_size, accel=self.accel, stride=self.accel)
-        Y = [(A@w).view(1, self.coils, -1, nc) for w in self.weights]
+        #Y = [(A@w).view(1, self.coils, -1, nc) for w in self.weights]
+        Y = [(A@w).view(1, self.coils, nr, -1) for w in self.weights]
         out = torch.zeros_like(Y[0])
         for rfe, rpe in self.start_inds:
             print(Y[rfe*self.accel[1]+rpe].shape)
