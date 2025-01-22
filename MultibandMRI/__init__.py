272a94b8f3db22ac765251b111e6bcf1febf140c
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-22 08:40:24 -0600

    in-plane grappa implementation 2

--- MultibandMRI/__init__.py
+++ MultibandMRI/__init__.py
@@ -1,3 +1,4 @@
 from .utils import get_kernel_patches, get_kernel_points, get_kernel_shifts, get_num_interpolated_points, interp_to_matrix_size, fft1d, fft2d, ifft1d, ifft2d
 from .slice_grappa import slice_grappa
 from .split_slice_grappa import split_slice_grappa
+from .grappa import grappa
