d2ea798350371150ec7c1f74af7ef572a08d8ab5
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-22 12:58:27 -0600

    grappa debug 2

--- MultibandMRI/grappa.py
+++ MultibandMRI/grappa.py
@@ -32,7 +32,8 @@ class grappa:
         # get the source data
         A = get_kernel_patches(calib_data, kernel_size=self.kernel_size, accel=self.accel)
         self.kernel_shifts, self.start_inds, self.eff_kernel_size = get_kernel_shifts(self.kernel_size, self.accel) 
-
+        print(self.kernel_shifts)
+        
         # l2 regularization 
         AH = A.conj().transpose(2,3)
         _,S,_ = torch.linalg.svd(A, full_matrices=False)
