8b1b26a645d95d6257009ea9f7019792f726ae05
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-24 21:24:24 -0600

    sms=4 sense grappa debug 4

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -57,7 +57,7 @@ class sense_grappa:
     def apply(self, inp_data):
 
         # readout FOV of extended-FOV images is no longer centered for an even number of simultaneously excited slices. add FOV/2 shift here
-        # if self.sms % 2 == 0: inp_data[:,:,1::2,:] = inp_data[:,:,1::2,:] * torch.exp(torch.tensor([1j], dtype=torch.complex64, device=inp_data.device))
+        if self.sms % 2 == 0: inp_data[:,:,1::2,:] = inp_data[:,:,1::2,:] * torch.exp(torch.tensor([1j], dtype=torch.complex64, device=inp_data.device))
 
         # zero-fill data 
         data = torch.zeros((inp_data.shape[0], inp_data.shape[1], self.sms*inp_data.shape[2], inp_data.shape[3]), dtype=inp_data.dtype, device=inp_data.device)
