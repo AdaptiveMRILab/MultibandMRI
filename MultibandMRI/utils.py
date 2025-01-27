f942c428e01b7a126225a39842c15402c2902e2e
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-27 15:06:17 -0600

    slice raki implementation 5

--- MultibandMRI/utils.py
+++ MultibandMRI/utils.py
@@ -122,8 +122,8 @@ class complex_mlp(torch.nn.Module):
 
     def forward(self, x):
         for n in range(self.num_layers):
-            xr = self.layers_real(x.real) - self.layers_imag(x.imag)
-            xi = self.layers_real(x.imag) + self.layers_imag(x.real)
+            xr = self.layers_real[n](x.real) - self.layers_imag[n](x.imag)
+            xi = self.layers_real[n](x.imag) + self.layers_imag[n](x.real)
             x = torch.complex(xr, xi) 
             if n < self.num_layers - 1:
                 x = self.crelu(x)
