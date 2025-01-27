2203239dbf44a748c1c8f4bd3606f4e85ae2659a
nmickevicius <nikolaimickevicius@gmail.com>
2025-01-27 15:01:35 -0600

    slice raki implementation 2

--- MultibandMRI/slice_raki.py
+++ MultibandMRI/slice_raki.py
@@ -77,7 +77,7 @@ class slice_raki:
             slice_model_paths = []
             for s in range(self.sms):
                 model_path = os.path.join(self.recon_folder, 'model_shift%i_slice%i.pt'%(self.kernel_shifts.index(shifts), s))
-                X = torch.cat([A[s,n,:,:] for n in range(self.coils)], dim=1)   # [observations, prod(kernel_size)*coils*coils]
+                X = torch.cat([A[0,n,:,:] for n in range(self.coils)], dim=1)   # [observations, prod(kernel_size)*coils*coils]
                 Y = rhs[s,:,:,0].permute(1,0)                                   # [observations, coils]
                 _, train_loss, val_loss  = train_complex_mlp(X, Y, model_path, self.train_split, 
                                           num_layers=self.num_layers, hidden_size=self.hidden_size, 
@@ -105,7 +105,7 @@ class slice_raki:
         for k in range(len(self.start_inds)):
             rfe, rpe = self.start_inds[k]
             for s in range(self.sms):
-                X = torch.cat([A[s,n,:,:] for n in range(self.coils)], dim=1)
+                X = torch.cat([A[0,n,:,:] for n in range(self.coils)], dim=1)
                 model = load_complex_mlp(self.model_paths[k][s], X.shape[1], self.coils, num_layers=self.num_layers, hidden_size=self.hidden_size).to(X.device)
                 pred = model(X).permute(1,0).view(self.coils, nr, -1)
                 if self.learn_residual:
