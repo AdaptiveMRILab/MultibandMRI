579e33552c75e49d174aef12e62c3e9efd93091d
Tyler Gallun <tgallun1@gmail.com>
2025-02-07 12:28:19 -0600

    Debugging 1

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -57,8 +57,13 @@ class sense_grappa:
 
     def apply(self, inp_data):
 
-        # readout FOV of extended-FOV images is no longer centered for an even number of simultaneously excited slices. add FOV/2 shift here
-        if self.sms % 2 == 0: inp_data[:,:,1::2,:] = inp_data[:,:,1::2,:] * np.exp(1j*np.pi)
+        # # readout FOV of extended-FOV images is no longer centered for an even number of simultaneously excited slices. add FOV/2 shift here
+        # if self.sms % 2 == 0: inp_data[:,:,1::2,:] = inp_data[:,:,1::2,:] * np.exp(1j*np.pi)
+        
+        # Apply FOV shift correction for SMS and acceleration factors
+        for slice_idx in range(self.sms):
+            phase_shift = np.exp(1j * np.pi * slice_idx / (self.sms * self.accel[0]))
+            inp_data[:, :, slice_idx::self.sms, :] *= phase_shift
 
         # zero-fill data 
         data = torch.zeros((inp_data.shape[0], inp_data.shape[1], self.sms*inp_data.shape[2], inp_data.shape[3]), dtype=inp_data.dtype, device=inp_data.device)
