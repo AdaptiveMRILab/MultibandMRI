7ae079bbcabaa1f889705b1ccaa4015927568189
Tyler Gallun <tgallun1@gmail.com>
2025-02-13 14:05:43 -0600

    Debugging band-aid fix 2

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -60,7 +60,7 @@ class sense_grappa:
         # readout FOV of extended-FOV images is no longer centered for an even number of simultaneously excited slices. add FOV/2 shift here
         if self.sms % 2 == 0: inp_data[:,:,1::2,:] = inp_data[:,:,1::2,:] * np.exp(1j*np.pi)
         print('test1')
-        if self.accel[0] == 3: 
+        if self.accel[1] == 3: 
             inp_data[:,:,:,1::3] = inp_data[:,:,:,1::3] * np.exp(-1j*np.pi)
             inp_data[:,:,:,2::3] = inp_data[:,:,:,2::3] * np.exp(1j*np.pi)
 
