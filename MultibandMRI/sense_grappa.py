f78c50c29e7550307f6858dc942298777c6b91a1
Tyler Gallun <tgallun1@gmail.com>
2025-02-13 14:18:19 -0600

    Debugging band-aid fix 15

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -61,7 +61,7 @@ class sense_grappa:
         if self.sms % 2 == 0: inp_data[:,:,1::2,:] = inp_data[:,:,1::2,:] * np.exp(1j*np.pi)
         if self.accel[1] == 3: 
             inp_data[:,:,:,1::3] = inp_data[:,:,:,1::3] * 0 #np.exp(1j*np.pi)
-            inp_data[:,:,:,2::3] = inp_data[:,:,:,2::3] * np.exp(-1j*np.pi)
+            inp_data[:,:,:,2::3] = inp_data[:,:,:,2::3] * 0 #np.exp(-1j*np.pi)
 
         # zero-fill data 
         data = torch.zeros((inp_data.shape[0], inp_data.shape[1], self.sms*inp_data.shape[2], inp_data.shape[3]), dtype=inp_data.dtype, device=inp_data.device)
