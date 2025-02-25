55c90d4fac87655ce105c42bc6506c44025a1542
Tyler Gallun <tgallun1@gmail.com>
2025-02-25 12:47:35 -0600

    Reverted print statements (troubleshooting prints are still commented out)

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -43,7 +43,6 @@ class sense_grappa:
         # print('Troubleshooting: Kernel patches shape: ', A.shape)
         # print('Troubleshooting: Sample kernel patches: ', A[0,0,:,:])
         # print('Troubleshooting: Kernel shifts: ', self.kernel_shifts)
-        print(self.accel)
 
         # l2 regularization 
         AH = A.conj().transpose(2,3)
