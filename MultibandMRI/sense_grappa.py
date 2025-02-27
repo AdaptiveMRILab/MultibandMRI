cbba3f25d9a006079a88021966ba58c3b4582468
Tyler Gallun <tgallun1@gmail.com>
2025-02-27 11:53:58 -0600

    Returned line to normal

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -109,7 +109,6 @@ class sense_grappa:
 
         # Troubleshooting: plot the kspace
         print(slc_ksp.shape)
-        slc_ksp[0,0,:,64] = 0
         plt.figure()
         plt.title('K-space plot')
         plt.imshow(np.log10(np.abs(slc_ksp[0,0,:,:].cpu().numpy())))
