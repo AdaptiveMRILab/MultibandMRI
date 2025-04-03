dbc8a5c75adbea732fb6792dab5461784f477624
Tyler Gallun <tgallun1@gmail.com>
2025-04-03 12:01:22 -0500

    Troubleshooting 5

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -63,22 +63,22 @@ class sense_grappa:
         # handling matrix sizes not evenly divisible by acceleration factor 
         phase_matrix_size = inp_data.shape[3]
         if inp_data.shape[3] % self.accel[1]:
-            npad = self.accel[1] - (inp_data.shape[3] % self.accel[1])
-            z = torch.zeros((inp_data.shape[0],inp_data.shape[1],inp_data.shape[2],npad), dtype=inp_data.dtype, device=inp_data.device)
-            inp_data = torch.cat([inp_data, z], dim=3)
-            print("Modified inp_data shape: ", inp_data.shape)
-
-            # flag = 1
-            # print("Data shape: ", inp_data.shape[3], "Acceleration: ", self.accel[1])
             # npad = self.accel[1] - (inp_data.shape[3] % self.accel[1])
-            # npad = inp_data.shape[3] % self.accel[1]
-            # print("npad: ", npad)
-            # front_pad = int(npad/2) # Try even padding
-            # back_pad = npad - front_pad # Try even padding
-            # z_front = torch.zeros((inp_data.shape[0],inp_data.shape[1],inp_data.shape[2], front_pad), dtype=inp_data.dtype, device=inp_data.device)
-            # z_back = torch.zeros((inp_data.shape[0],inp_data.shape[1],inp_data.shape[2], back_pad), dtype=inp_data.dtype, device=inp_data.device)
-            # inp_data = torch.cat([z_front, inp_data, z_back], dim=3)
-            # print(inp_data.shape)
+            # z = torch.zeros((inp_data.shape[0],inp_data.shape[1],inp_data.shape[2],npad), dtype=inp_data.dtype, device=inp_data.device)
+            # inp_data = torch.cat([inp_data, z], dim=3)
+            # print("Modified inp_data shape: ", inp_data.shape)
+
+            flag = 1
+            print("Data shape: ", inp_data.shape[3], "Acceleration: ", self.accel[1])
+            npad = self.accel[1] - (inp_data.shape[3] % self.accel[1])
+            npad = inp_data.shape[3] % self.accel[1]
+            print("npad: ", npad)
+            front_pad = int(npad/2) # Try even padding
+            back_pad = npad - front_pad # Try even padding
+            z_front = torch.zeros((inp_data.shape[0],inp_data.shape[1],inp_data.shape[2], front_pad), dtype=inp_data.dtype, device=inp_data.device)
+            z_back = torch.zeros((inp_data.shape[0],inp_data.shape[1],inp_data.shape[2], back_pad), dtype=inp_data.dtype, device=inp_data.device)
+            inp_data = torch.cat([z_front, inp_data, z_back], dim=3)
+            print(inp_data.shape)
 
         # zero-fill data 
         data = torch.zeros((inp_data.shape[0], inp_data.shape[1], self.sms*inp_data.shape[2], inp_data.shape[3]), dtype=inp_data.dtype, device=inp_data.device)
