6a76538f731c8b6085fc45134edb3e8d8638ff7c
Tyler Gallun <tgallun1@gmail.com>
2025-04-04 12:46:10 -0500

    Put padding back in

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -62,10 +62,10 @@ class sense_grappa:
 
         # handling matrix sizes not evenly divisible by acceleration factor 
         phase_matrix_size = inp_data.shape[3]
-        # if inp_data.shape[3] % self.accel[1]:
-        #     npad = self.accel[1] - (inp_data.shape[3] % self.accel[1])
-        #     z = torch.zeros((inp_data.shape[0],inp_data.shape[1],inp_data.shape[2],npad), dtype=inp_data.dtype, device=inp_data.device)
-        #     inp_data = torch.cat([inp_data, z], dim=3)
+        if inp_data.shape[3] % self.accel[1]:
+            npad = self.accel[1] - (inp_data.shape[3] % self.accel[1])
+            z = torch.zeros((inp_data.shape[0],inp_data.shape[1],inp_data.shape[2],npad), dtype=inp_data.dtype, device=inp_data.device)
+            inp_data = torch.cat([inp_data, z], dim=3)
 
         # zero-fill data 
         data = torch.zeros((inp_data.shape[0], inp_data.shape[1], self.sms*inp_data.shape[2], inp_data.shape[3]), dtype=inp_data.dtype, device=inp_data.device)
