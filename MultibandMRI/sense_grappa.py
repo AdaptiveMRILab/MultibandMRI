d356dca90a001359c96300c2714198574a403da5
Tyler Gallun <tgallun1@gmail.com>
2025-04-07 10:37:28 -0500

    Apparently I should try random things more often

--- MultibandMRI/sense_grappa.py
+++ MultibandMRI/sense_grappa.py
@@ -54,15 +54,6 @@ class sense_grappa:
         for shifts in self.kernel_shifts:
             b = get_kernel_points(data, shifts=shifts, kernel_size=self.kernel_size, accel=self.accel)
             self.weights.append(AHA_inv @ (AH @ b))
-    
-    # Current notes and deductions:
-    # The error has to be in the apply stage if the code works for other SMS / acceleration factors
-    # The code only malfunctions if inp_data.shape[3] % accel[1] != 0; so something with the acceleration is messing with things
-    # The accel tuple is (sms, R), whereas slice_grappa and split_slice_grappa use (1, R) as the accel tuple (in application)
-    # get_kernel_patches and get_kernel_points both use this acceleration tuple, but I don't think they're wrong due to me thinking the calibration is done correctly
-    # interp_to_matrix_size doesn't use the acceleration and pads to the right size (it's wierd since the lines of kspace are there, but low-value, but other
-    # reconstruction functions have the lines removed in kspace (since you can't take the log of 0))
-    # get_num_interpolated_points uses the accel tuple, this could be the source of the error
 
     def apply(self, inp_data):
 
@@ -101,7 +92,7 @@ class sense_grappa:
         # remove any extra zero padding lines that were added above
         data = data[...,:phase_matrix_size]
 
-        # data consistency
+        # # data consistency
         # out[torch.abs(data) > 0.0] = data[torch.abs(data) > 0.0]
 
         # bring to the image domain and crop slices
@@ -112,51 +103,3 @@ class sense_grappa:
         rss = torch.sqrt(torch.sum(torch.abs(img * img.conj()), dim=1))
 
         return slc_ksp, rss
-    
-    # ORIGINAL APPLY CODE
-    # def apply(self, inp_data):
-
-    #     # readout FOV of extended-FOV images is no longer centered for an even number of simultaneously excited slices. add FOV/2 shift here
-    #     if self.sms % 2 == 0: inp_data[:,:,1::2,:] = inp_data[:,:,1::2,:] * np.exp(1j*np.pi)
-
-    #     # handling matrix sizes not evenly divisible by acceleration factor 
-    #     phase_matrix_size = inp_data.shape[3]
-    #     if inp_data.shape[3] % self.accel[1]:
-    #         npad = inp_data.shape[3] % self.accel[1]
-    #         z = torch.zeros((inp_data.shape[0],inp_data.shape[1],inp_data.shape[2],npad), dtype=inp_data.dtype, device=inp_data.device)
-    #         inp_data = torch.cat([inp_data, z], dim=3)
-
-    #     # zero-fill data 
-    #     data = torch.zeros((inp_data.shape[0], inp_data.shape[1], self.sms*inp_data.shape[2], inp_data.shape[3]), dtype=inp_data.dtype, device=inp_data.device)
-    #     data[:,:,::self.sms,:] = inp_data
-
-    #     # figure out number of interpolated points along each dimension 
-    #     nr, nc = get_num_interpolated_points(data.shape, self.kernel_size, self.accel)
-
-    #     # interpolate the missing points
-    #     A = get_kernel_patches(data, kernel_size=self.kernel_size, accel=self.accel, stride=self.accel)
-    #     Y = [(A@w).view(1, self.coils, nr, nc) for w in self.weights]
-
-    #     out = torch.zeros((1, self.coils, self.accel[0]*nr, self.accel[1]*nc), dtype=inp_data.dtype, device=inp_data.device)
-    #     for rfe, rpe in self.start_inds:
-    #         out[:,:,rfe::self.accel[0],rpe::self.accel[1]] = Y[rfe*self.accel[1]+rpe]
-
-    #     # final interpolation 
-    #     if self.final_matrix_size is not None:
-    #         adjusted_matrix_size = (self.sms*self.final_matrix_size[0], self.final_matrix_size[1])
-    #         out = interp_to_matrix_size(out, adjusted_matrix_size)
-
-    #     # remove any extra zero padding lines that were added above
-    #     data = data[...,:phase_matrix_size]
-
-    #     # data consistency
-    #     out[torch.abs(data) > 0.0] = data[torch.abs(data) > 0.0]
-
-    #     # bring to the image domain and crop slices
-    #     nread = inp_data.shape[2]
-    #     img = ifft2d(out, dims=(2,3))
-    #     img = torch.stack([img[0,:,n*nread:(n+1)*nread,:] for n in range(self.sms)], axis=0)
-    #     slc_ksp = fft2d(img, dims=(2,3))
-    #     rss = torch.sqrt(torch.sum(torch.abs(img * img.conj()), dim=1))
-
-    #     return slc_ksp, rss
--- MultibandMRI/utils.py
+++ MultibandMRI/utils.py
@@ -79,12 +79,7 @@ def interp_to_matrix_size(inp: Tensor,
     colpst = colpad//2
     colpre = colpad - colpst
     out = torch.nn.functional.pad(inp, (colpre, colpst, rowpre, rowpst), mode='constant', value=0) # original 
-
-    print("Interp to matrix size printouts:")
-    print("Input shape: ", inp.shape)
-    print("Rowpad: ", rowpad)
-    print("Colpad: ", colpad)
-    print("Output shape: ", out.shape)
+    
     return out
 
 def get_num_interpolated_points(shp: Tuple,
